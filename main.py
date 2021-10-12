5106d702a8e218ebb02ef3c965877b0467e883ec
amaxaaaaacko <amaxaaaaacko@gmail.com>
2021-10-12 04:49:55 +0300

    connect_mongo

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from pymongo import MongoClient
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 from config import API_TOKEN
+from pprint import pprint
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+
+client = MongoClient('mongodb+srv://<username>:<password>@cluster0.mydh0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+db = client.admin
+serverStatusResult = db.command("serverStatus")
+pprint(serverStatusResult)
